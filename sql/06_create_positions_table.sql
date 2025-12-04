9f453c29487d631179c5c500020ec8aecf7fa85b
Owen K <81033833+owenrbk@users.noreply.github.com>
2025-12-04 12:44:23 -0500

    Update and rename 06_create_player_history.sql to 06_create_positions_table.sql

---
+++ sql/06_create_positions_table.sql
@@ -0,0 +1,11 @@
+-- 06_create_positions_table.sql
+-- Organizes and defines the 4 positions and their rules
+
+CREATE TABLE IF NOT EXISTS staging.positions (
+    position_id NUMERIC(1) PRIMARY KEY,
+    name VARCHAR(15),
+    name_short CHAR(3),
+    squad_select NUMERIC(1),
+    squad_min_play NUMERIC(1),
+    squad_max_play NUMERIC(1),
+);
