32702a280c3ac4d57b5a8905f962c8b7b5a848f2
Owen K <81033833+owenrbk@users.noreply.github.com>
2025-12-04 14:26:30 -0500

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -68,14 +68,14 @@ CREATE TABLE IF NOT EXISTS staging.gameweeks (
     gameweek_id NUMERIC(2) PRIMARY KEY,
     average_score NUMERIC(3),
     highest_score NUMERIC(3),
-    ranked_count NUMERIC(15),
+    ranked_count NUMERIC(10),
     chip_plays TEXT,
     most_selected NUMERIC(4),
     most_transferred_in NUMERIC(4),
     most_captained NUMERIC(4),
     most_vice_captained NUMERIC(4),
     top_player NUMERIC(4),
-    transfers_made NUMERIC(15)
+    transfers_made NUMERIC(10)
 );
 
 CREATE TABLE IF NOT EXISTS staging.chips (
