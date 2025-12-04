0abdc0ea4ae54b42f8c0c18ea1aca6c0088dc804
Owen K <81033833+owenrbk@users.noreply.github.com>
2025-12-04 12:37:45 -0500

    Update 05_create_fixtures_table.sql

--- sql/05_create_fixtures_table.sql
+++ sql/05_create_fixtures_table.sql
@@ -1,13 +1,16 @@
 -- 05_create_fixtures_table.sql
 -- Parses fixture data from raw_fpl JSON
 
-CREATE TABLE fixtures (
-    fixture_id SERIAL PRIMARY KEY,
-    event INT,
-    home_team INT,
-    away_team INT,
-    kickoff_time TIMESTAMP,
-    finished BOOLEAN,
-    home_score INT,
-    away_score INT
+CREATE TABLE IF NOT EXISTS staging.gameweeks (
+    gameweek_id NUMERIC(2) PRIMARY KEY,
+    average_score NUMERIC(3),
+    highest_score NUMERIC(3),
+    ranked_count NUMERIC(10),
+    chip_plays TEXT,
+    most_selected INT,
+    most_transferred_in INT,
+    most_captained INT,
+    most_vice_captained INT,
+    top_player INT,
+    transfers_made INT
 );
