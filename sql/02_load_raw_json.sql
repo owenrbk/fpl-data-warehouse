5f2267035c3a28fe1da2c5119ae58ea7a9a45084
Owen K <81033833+owenrbk@users.noreply.github.com>
2025-12-04 12:47:43 -0500

    Delete sql/02_load_raw_json.sql

--- sql/02_load_raw_json.sql
+++
@@ -1,6 +0,0 @@
--- 02_load_raw_json.sql
--- Loads raw JSON data into raw_fpl
--- This can be run after placing your JSON files in the local data folder
-
-INSERT INTO raw_fpl (source_filename, json_data)
-VALUES ('bootstrap-static.json', '<replace_with_JSON_data_here>');
