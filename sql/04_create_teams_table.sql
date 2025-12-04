0fc4d4e0e216920c12f266e549482f06a2c871d2
Owen K <81033833+owenrbk@users.noreply.github.com>
2025-12-04 11:58:47 -0500

    Update 04_create_teams_table.sql

--- sql/04_create_teams_table.sql
+++ sql/04_create_teams_table.sql
@@ -1,9 +1,18 @@
 -- 04_create_teams_table.sql
 -- Extracts team information from raw_fpl JSON
 
-CREATE TABLE teams (
-    team_id INT PRIMARY KEY,
-    team_name TEXT,
-    short_name TEXT,
-    code INT
+CREATE TABLE IF NOT EXISTS staging.teams (
+    team_id NUMERIC(3) PRIMARY KEY,
+    team_name VARCHAR(50),
+    short_name CHAR(3),
+    position NUMERIC(2),
+    strength NUMERIC(4),
+    strength_overall_home NUMERIC(4),
+    strength_overall_away NUMERIC(4),
+    strength_attack_home NUMERIC(4),
+    strength_attack_away NUMERIC(4),
+    strength_defense_home NUMERIC(4),
+    strength_defense_away NUMERIC(4),
+    pulse_id NUMERIC(3),
+    code NUMERIC(3)
 );
