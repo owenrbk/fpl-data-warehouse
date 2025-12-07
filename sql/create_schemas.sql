c59c3626e167cc0dbca1af19e7205e27be6a2430
Owen K <81033833+owenrbk@users.noreply.github.com>
2025-12-07 13:39:28 -0500

    Update create_schemas.sql

--- sql/create_schemas.sql
+++ sql/create_schemas.sql
@@ -1,6 +1,5 @@
 -- create_schemas.sql
--- Creates separate schemas for different stages of the data pipeline
+-- Creates schemas for two different stages of the data pipeline
 
-CREATE SCHEMA IF NOT EXISTS raw;
 CREATE SCHEMA IF NOT EXISTS core;
 CREATE SCHEMA IF NOT EXISTS analytics;
