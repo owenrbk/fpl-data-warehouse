9af9fb91bb46d5fdca58369685dca283f956f991
Owen K <81033833+owenrbk@users.noreply.github.com>
2025-12-09 14:46:36 -0500

    Delete etl_fpl directory

--- etl_fpl/extract.py
+++
@@ -1,9 +0,0 @@
-import requests
-import logging
-from config import FPL_API_URL
-
-def extract_fpl_data():
-    logging.info("Extracting data from FPL API...")
-    response = requests.get(FPL_API_URL)
-    response.raise_for_status()
-    return response.json()
